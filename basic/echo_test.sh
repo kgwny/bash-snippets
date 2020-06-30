dcf8d895a386be8be30238b2ae01198d7f53a20c
kgwny <kigawanaoya@gmail.com>
2020-06-30 21:29:41 +0900

    Add some snippets

---
+++ basic/echo_test.sh
@@ -0,0 +1,12 @@
+
+echo "start"
+
+# 改行なし(macだと改行されてしまう)
+echo -n "hoge ika tako"
+
+# macで改行なしを実現するにはこのように記述する
+/bin/echo -n ABC
+
+echo "foo bar baz"
+
+echo "end"
---
+++ basic/function_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 関数の定義
+hello_world () {
+    echo "Hello, World!"
+}
+
+# 関数の呼び出し
+hello_world
