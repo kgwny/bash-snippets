4af92987dcab32833346227b538f9f13c6d2f79c
kgwny <kigawanaoya@gmail.com>
2020-08-24 21:37:12 +0900

    add some snippets

---
+++ basic/countdown.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 10 9 8 7 6 5 4 3 2 1
+for (( count=10; count>0; count-- ))
+do
+echo "$count"
+done
---
+++ basic/countup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 0 1 2 3 4 5 6 7 8 9
+for (( count=0; count<10; count++ ))
+do
+echo "$count"
+done
