2e1a6d67962921f50667817eb6ebcfc3e9614b0d
kgwny <kigawanaoya@gmail.com>
2021-06-06 23:27:13 +0900

    add snippet

---
+++ loop/for1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for i in "$@"
+do
+  echo "$i"
+done
+
+# sh for1.sh 1 2 3 4
+# 1
+# 2
+# 3
+# 4
---
+++ loop/for2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for i in {1..10}
+do
+    echo "$i"
+done
+
+# $ sh for2.sh
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
